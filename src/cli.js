e27a31fbaedd44830afb3c40f7e128b3a81878a5
rusmurs <myrsaloff@gmail.com>
2022-06-28 23:54:55 +0300

    creating a logical function and calling

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+
+export function userName () {
+    console.log('Welcome to the Brain Games!');
+    const nameUser = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${nameUser}!`);
+};
