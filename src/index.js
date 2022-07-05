763584ca2dc6528a7928382c3e209be1be0409d9
rusmurs <myrsaloff@gmail.com>
2022-07-05 21:49:02 +0300

    add index file, random file and refactoring

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const foundationGames = (gistGames, questionsAndAnswers) => {
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+
+  console.log(gistGames);
+
+  const counterRound = 3;
+  for (let i = 1; i <= counterRound; i += 1) {
+    const [questions, correctAnswers] = questionsAndAnswers();
+    console.log(`Question: ${questions}`);
+    const yourAnswer = readlineSync.question('Your answer: ');
+
+    if (yourAnswer.toLocaleLowerCase() !== correctAnswers) {
+      console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${correctAnswers}.\nLet's try again, ${nameUser}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${nameUser}!`);
+};
+
+export default foundationGames;
